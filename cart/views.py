42fd491d3c9106a07695818073f38fbaad158082
crishelpc <202280108@psu.palawan.edu.ph>
2024-01-01 17:51:36 +0800

    modify

--- cart/views.py
+++ cart/views.py
@@ -8,25 +8,24 @@ def cart_summary(request):
 
 
 def cart_add(request): 
-    #get the cart
+    #get the cart 
     cart = Cart(request)
-    #test for POST
-    if request.POST.get('action') == 'post': 
-        #get stuff
+    #test the POST
+    if request.POST.get('action') == 'post':
+        #get stuff 
         product_id = int(request.POST.get('product_id'))
-      
-        #lookup prod in database
+        #lookup product in database 
         product = get_object_or_404(Product, id=product_id)
-        #Save to session
-        cart.add(product=product)
-        #Crt quantity
-        cart_quantity = cart.__len__()
-        
-        #Return response
-        # response = JsonResponse({'Product Name: ': product.name})
-        response = JsonResponse({ 'qty': cart_quantity})
-        return response
 
+        #Save to Session 
+        cart.add(product=product)
+
+        #Get Cart Quantity
+        cart_quantity = cart.__len__()
+
+        #return response
+        response = JsonResponse ({'qty': cart_quantity })
+        return response
 
 def cart_delete(request): 
     pass
